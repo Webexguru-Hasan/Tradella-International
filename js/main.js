865ab2ab47f0e614bab872f1c384d3eba67aaf3e
webexguruhasan <webexguruhasan95@gmail.com>
2024-05-09 16:30:43 +0600

    created visited page functionality using javascript also used BEM methodology on pre-header and main-header section

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// Visited-page-functionality-here
+
+document.addEventListener("DOMContentLoaded", function() {
+    var currentPage = window.location.pathname.split("/").pop();
+    var navLinks = document.querySelectorAll(".main-header__nav-link");
+
+    navLinks.forEach(function(link) {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
+});
